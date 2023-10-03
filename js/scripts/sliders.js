e94df25f4d1aeb3339f26b0796ef8c3c1f8b15da
Egor Skleynov <yxllwllxy@gmail.com>
2023-10-03 18:23:41 +0800

    add background autoslider

--- js/scripts/sliders.js
+++ js/scripts/sliders.js
@@ -1,31 +1,16 @@
-const slider_reviews = document.querySelector('.swiper-container_reviews');
+const slider_photos = document.querySelector('.swiper');
 
-let swiper_reviews = new Swiper(slider_reviews, {
-    slidesPerView: 1,
-    spaceBetween: 10,
+let swiper = new Swiper(slider_photos, {
     loop: true,
-    effect: 'flip',
-    speed: 600,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        type: 'bullets',
+    allowTouchMove: false,
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
-})
-
-const slider_photos = document.querySelector('.swiper-container_photos');
-
-let swiper_photos = new Swiper(slider_photos, {
-    slidesPerView: 3,
-    spaceBetween: 951,
-    loop: true,
-    speed: 600,
-    navigation: {
-        nextEl: '.swiper-button-next_photo',
-        prevEl: '.swiper-button-prev_photo',
     },
 })
