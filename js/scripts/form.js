893c39af18d9d837d7b7789389ccd3de8fd4590e
Egor Skleynov <yxllwllxy@gmail.com>
2023-10-04 02:36:57 +0800

    add form validation

--- js/scripts/form.js
+++ js/scripts/form.js
@@ -1,81 +1,49 @@
-console.log('Init!');
+const form = document.querySelector('.sidebar__form')
+const telSelector = form.querySelector('input[type="tel"]')
+const inputMask = new Inputmask('+7 (999) 999-99-99')
+inputMask.mask(telSelector)
 
-const form = document.querySelector('.modal__form');
-const telSelector = form.querySelector('input[type="tel"]');
-const inputMask = new Inputmask('+7 (999) 999-99-99');
-inputMask.mask(telSelector);
-
-const validation = new JustValidate('.modal__form');
+const validation = new JustValidate('.sidebar__form')
 
 validation
-    .addField('.input-name', [
-        {
-            rule: 'minLength',
-            value: 3,
+.addField('.input-name', [
+    {
+        rule: 'minLength',
+        value: 3,
+        errorMessage: 'Поле должно содержать минимум 3 символа!'
+    },
+    {
+        rule: 'maxLength',
+        value: 30,
+    },
+    {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Введите имя!'
+    }
+])
+.addField('.input-tel', [
+    {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Введите телефон!',
+    },
+    {
+        rule: 'function',
+        validator: function () {
+            const phone = telSelector.inputmask.unmaskedvalue()
+            return phone.length === 10
         },
-        {
-            rule: 'maxLength',
-            value: 30,
-        },
-        {
-            rule: 'required',
-            value: true,
-            errorMessage: 'Enter your name!'
-        }
-    ])
-    .addField('.input-email', [
-        {
-            rule: 'required',
-            value: true,
-            errorMessage: 'Email is required!',
-        },
-        {
-            rule: 'email',
-            value: true,
-            errorMessage: 'Enter a valid email address!',
-        },
-    ])
-    .addField('.input-tel', [
-        {
-            rule: 'required',
-            value: true,
-            errorMessage: 'Phone is required!',
-        },
-        {
-            rule: 'function',
-            validator: function () {
-                const phone = telSelector.inputmask.unmaskedvalue();
-                return phone.length === 10;
-            },
-            errorMessage: 'Enter a valid phone number!',
-        },
-    ])
-    .addField('.input-date', [
-        {
-            rule: 'required',
-            value: true,
-            errorMessage: 'Date is required!',
-        }
-    ])
-    .onSuccess((event) => {
-        console.log('Validation passes and form submitted', event);
-
-        let formData = new FormData(event.target);
-
-        console.log(...formData);
-
-        let xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    console.log('Sent!');
-                }
-            }
-        }
-
-        xhr.open('POST', '../../php/sendmail.php', true);
-        xhr.send(formData);
-
-        event.target.reset();
-    });
+        errorMessage: 'Введите настоящий номер телефона!',
+    },
+])
+.addField('.input-checkbox', [
+    {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Согласитесь с правилами!'
+    }
+])
+.onSuccess((event) => {
+    console.log('Validation passes and form submitted', event)
+})
